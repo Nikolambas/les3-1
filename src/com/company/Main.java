fcd589c743b9c6ffd0353eb016b0e0f4ed8151ba
Nikolambas <88347156+Nikolambas@users.noreply.github.com>
2021-11-23 20:06:15 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.util.ArrayList;
 
 public class Main {
 
@@ -8,22 +7,5 @@ public class Main {
     public static void main(String[] args) {
     }
 
-    public static <T> void reverseObj(T [] oneObj,T [] twoObj,int numberObj) {
-        try {
-            T one = oneObj[numberObj];
-            oneObj[numberObj] = twoObj[numberObj];
-            twoObj[numberObj] = one;
-        } catch (ArrayStoreException e) {
-            System.out.println("Выбранные массивы не схожи по объектам");
-        }
-    }
-
-    public static <A> ArrayList<A> arrToArrayList(A [] arr){
-        ArrayList<A>arrayList = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            arrayList.add(arr[i]);
-        }
-        return arrayList;
-    }
 }
 
