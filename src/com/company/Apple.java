3f47ca7301b24297e1c4f65101238196621110fc
Nikolambas <88347156+Nikolambas@users.noreply.github.com>
2021-11-23 20:05:14 +0300

    Update Apple.java

--- src/com/company/Apple.java
+++ src/com/company/Apple.java
@@ -1,15 +1,5 @@
 package com.company;
 
 public class Apple extends Fruit {
-    private final String name = "Apple";
-    private final Float weight = 1.0f;
-
-    public Float getWeight() {
-        return weight;
-    }
-
-
-    public String getName() {
-        return name;
-    }
+    
 }
